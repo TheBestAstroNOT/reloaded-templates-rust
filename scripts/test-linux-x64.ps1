e214bd178b9c9f8ef8af08a28f3f13023b86f3a0
sewer56lol <sewer56lol@googlemail.com>
2023-07-14 05:31:31 +0100

    Update xPlat Build Scripts with Extra Platforms

--- scripts/test-linux-x64.ps1
+++ scripts/test-linux-x64.ps1
@@ -1,5 +1,13 @@
 # Make sure you have Docker/Podman first
+
+echo "Installing Cross Compiler"
 cargo install cross
-rustup target add i686-unknown-linux-gnu
-cross build --target i686-unknown-linux-gnu
-cross test --target i686-unknown-linux-gnu
+
+echo "Adding Linux (x64) Target"
+rustup target add x86_64-unknown-linux-gnu
+
+echo "Building for Linux (x64)"
+cross build --target x86_64-unknown-linux-gnu
+
+echo "Testing Linux (x64)"
+cross test --target x86_64-unknown-linux-gnu
--- scripts/test-linux-x86.ps1
+++ scripts/test-linux-x86.ps1
@@ -1,5 +1,13 @@
 # Make sure you have Docker/Podman first
+
+echo "Installing Cross Compiler"
 cargo install cross
-rustup target add x86_64-unknown-linux-gnu
-cross build --target x86_64-unknown-linux-gnu
-cross test --target x86_64-unknown-linux-gnu
+
+echo "Adding Linux (x86) Target"
+rustup target add i686-unknown-linux-gnu
+
+echo "Building for Linux (x86)"
+cross build --target i686-unknown-linux-gnu
+
+echo "Testing Linux (x86)"
+cross test --target i686-unknown-linux-gnu
