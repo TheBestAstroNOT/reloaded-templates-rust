10eb3101c9e9b4fd231e90d28b6466070e834cfd
sewer56lol <sewer56lol@googlemail.com>
2023-07-16 21:20:03 +0100

    Added: Profiling Configuration & Guidance

--- templates/library/benches/my_benchmark.rs
+++ templates/library/benches/my_benchmark.rs
@@ -1,5 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+#[cfg(not(target_os = "windows"))]
+use pprof::criterion::{Output, PProfProfiler};
+
 fn fibonacci(n: u64) -> u64 {
     match n {
         0 => 1,
@@ -12,5 +15,18 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
-criterion_group!(benches, criterion_benchmark);
+#[cfg(not(target_os = "windows"))]
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = criterion_benchmark
+}
+
+#[cfg(target_os = "windows")]
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = criterion_benchmark
+}
+
 criterion_main!(benches);
