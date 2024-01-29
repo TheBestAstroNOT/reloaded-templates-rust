a967b8c1b3b0c67b4a4659b2a96e888f9e944aa3
Sewer56 <sewer56lol@googlemail.com>
2024-01-29 06:39:14 +0000

    Added: no_std support to template

--- templates/library/src/lib.rs
+++ templates/library/src/lib.rs
@@ -1,6 +1,11 @@
 //! # Some Cool Reloaded Library
 //! Here's the crate documentation.
-
+{%- if no-std-by-default %}
+#![cfg_attr(not(test), no_std)]
+{%- endif %}
+{%- if std-by-default %}
+#![cfg_attr(not(feature = "std"), no_std)]
+{%- endif %}
 {%- if build-c-libs %}
 pub mod exports;
 {%- endif %}
