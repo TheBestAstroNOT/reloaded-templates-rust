160826ff956a5fafb989d8e886d5a76012b21bf4
Sewer56 <sewer56lol@googlemail.com>
2024-02-04 17:04:50 +0000

    WIP: Adding .NET Bindings

---
+++ templates/library/build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Build time scripts go here. If you have nothing to do here, you can remove this file.
+{% if publish-crate-on-tag -%}
+    csbindgen::Builder::default()
+        .input_extern_file("src/exports.rs")
+        .csharp_dll_name("example")
+        .csharp_class_accessibility("public")
+        .csharp_namespace("{{crate_name}}.Net.Sys")
+        .generate_csharp_file("bindings/csharp/NativeMethods.g.cs")
+        .unwrap();
+{% endif %}
+}
