31df85723f5b40d3f58aaabe46559527065d5a91
Sewer56 <sewer56lol@googlemail.com>
2025-10-21 07:37:44 +0100

    Updated: Template flake config to accept wine.

--- templates/library/flake.nix
+++ templates/library/flake.nix
@@ -1,27 +1,29 @@
 # Flake for Windows cross-compilation testing with pthreads fix
 # Usage: nix develop .#windows && cargo test --target x86_64-pc-windows-gnu --release
 # Assumes cargo/rust are managed via rustup
+#
+# Uses rust-windows-gnu-helper-flake for Windows GNU cross-compilation environment
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-windows-gnu-helper-flake.url = "github:Sewer56/rust-windows-gnu-helper-flake";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-windows-gnu-helper-flake,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      mingwPkgs = nixpkgs.legacyPackages.${system}.pkgsCross.mingwW64;
+      winGnuEnv = rust-windows-gnu-helper-flake.lib.${system}.winGnuEnv;
+      winGnuPackages = rust-windows-gnu-helper-flake.lib.${system}.winGnuPackages;
     in {
-      devShells.windows = pkgs.mkShell {
-        CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUSTFLAGS = "-L native=${mingwPkgs.windows.pthreads}/lib";
-
-        buildInputs = [
-          mingwPkgs.buildPackages.gcc
-        ];
-      };
+      devShells.windows = pkgs.mkShell (winGnuEnv
+        // {
+          buildInputs = winGnuPackages;
+        });
     });
 }
